3c8dc4194b528f827b3ddef04b1dcd7b0e799e78
Julius <njmwas@gmail.com>
2025-03-24 16:50:12 +0300

    Allow cors

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
 server.use('/api', router)
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://njmwas.github.io')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
